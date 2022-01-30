bcef70e1b5cedc54087c5b75034274bc214579fe
aditya12311 <98684778+aditya12311@users.noreply.github.com>
2022-01-30 21:10:47 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,12 +6,12 @@ var db = require(__path + '/database/db');
 try {
 var kuhong = db.get('ojan'); // jan diubah
 } catch (e) {
-	console.log('WELCOME TO ZEROYT7 API!') // boleh diubah
+	console.log('WELCOME TO ADIT API!') // boleh diubah
 }
 
-var creatorList = ['Zero YT7']; // Nama Lu Ngab
+var creatorList = ['Adit API']; // Nama Lu Ngab
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)]; // Ini jan diubah
-var key = 'ZeroYT7' // Apikey Lu Ngab
+var key = 'Aditkey // Apikey Lu Ngab
 
 // Required Modules :
 var ffmpeg = require('fluent-ffmpeg');
