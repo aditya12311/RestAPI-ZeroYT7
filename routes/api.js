492a8a8246a0d2cc8144c975ce4fe37a821c71ca
aditya12311 <98684778+aditya12311@users.noreply.github.com>
2022-01-31 17:31:14 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,10 @@ var creatorList = ['Adit API']; // Nama Lu Ngab
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)]; // Ini jan diubah
 var key = 'Aditkey' // Apikey Lu Ngab
 
+var creatorList = ['Adit API];
+var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
+var key = 'zeroyt7' // boleh di ubah
+
 // Required Modules :
 var ffmpeg = require('fluent-ffmpeg');
 var brainly = require('brainly-scraper-v2');
